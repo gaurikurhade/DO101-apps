75f69a4e1b30e6bb4e7fd09f6944f7a3871a6b9f
gaurikurhade <gaurikurhade4@gmail.com>
2025-01-13 12:27:40 +0530

    updated app to v3

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -22,3 +22,12 @@ app.get('/', function (req, res) {
     res.send(response);
 
 });
+var response;
+
+app.get('/', function (req, res) {
+    response = 'This is version 3 of the app.' + '\n';
+
+    //send the response to the client
+    res.send(response);
+
+});
